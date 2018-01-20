e6e9abb6fa8e5433f6a35e1c51d399ba353d8616
Bennett Hardwick <bennetthardwick@icloud.com>
2018-01-20 21:42:22 +1000

    Created skeleton methods in Customer class

--- lib/Customer.ts
+++ lib/Customer.ts
@@ -1,7 +1,33 @@
+import { customers } from 'stripe';
+import { Charge } from './Charge';
+
 export class Customer {
 
-	constructor() {
-		
-	}
+  public id: string;
+
+  private _customer: customers.ICustomerCreationOptions;
+
+  constructor(source?: string) {
+      
+  }
+
+  source(source: string): Customer {
+    this._customer.source = source;
+    return this;
+  }
+
+  email(email: string): Customer {
+    this._customer.email = email;
+    return this;
+  }
+
+  description(description: string): Customer {
+    this._customer.description = description;
+    return this;
+  }
+
+  createCharge() {
+    return new Charge(null, this);
+  }
 
 }
