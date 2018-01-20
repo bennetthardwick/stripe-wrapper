79da719ebd296ea715dfdf1c10d04a230b535fc9
Bennett Hardwick <bennetthardwick@icloud.com>
2018-01-20 21:41:59 +1000

    Implemented Stripe charges

--- lib/Charge.ts
+++ lib/Charge.ts
@@ -3,40 +3,66 @@ import { charges, IResponseFn } from 'stripe';
 import { Customer } from './Customer';
 
 export class Charge {
-	
-	private customer: Customer;
-	private source: string;
+  
+  private customer: Customer;
+  private source: string;
 
-	private _charge: charges.IChargeCreationOptions;
+  private _charge: charges.IChargeCreationOptions = {
+    amount: 1000,
+    currency: 'usd',
+  };
 
-	constructor(source: string, customer?: Customer) {
-		this.customer = customer;
-		this.source = source;
-	}
+  constructor(source: string, customer?: Customer) {
+    if (customer) this._charge.customer = customer.id;
+    if (source) this._charge.source = source;
+    this._charge.source = source;
+    this._charge.currency = "usd";
+  }
 
-	amount(amount: number): Charge {
-		this._charge.amount = amount;
-		return this;
-	}
+  /**
+   * Set the amount to be charged.
+   * @param { amount: number } - Value of the transaction in cents.
+   */
+  amount(amount: number): Charge {
+    this._charge.amount = amount;
+    return this;
+  }
 
-	currency(currency: string): Charge {
-		this._charge.currency = currency;
-		return this;
-	}
+  /**
+   * Set the desired currency of the transaction.
+   * @param { currency: string } [usd] - Three-letter ISO currency code. 
+   */
+  currency(currency: string): Charge {
+    this._charge.currency = currency;
+    return this;
+  }
 
-	description(description: string): Charge {
-		this._charge.description = description;
-		return this;
-	}
+  /**
+   * Set the description of the transaction.
+   * @param { description: string } - Description of the transaction.
+   */
+  description(description: string): Charge {
+    this._charge.description = description;
+    return this;
+  }
 
-	options(options: charges.IChargeCreationOptions) {
-		this._charge = Object.assign<charges.IChargeCreationOptions, charges.IChargeCreationOptions>(this._charge, options);
-		return this;
-	}
+  /**
+   * Set various options of the transaction
+   * @param { options: charges.IChargeCreationOptions } - Object containing the transaction options.
+   */
+  options(options: charges.IChargeCreationOptions) {
+    this._charge = Object.assign<charges.IChargeCreationOptions, charges.IChargeCreationOptions>(this._charge, options);
+    return this;
+  }
 
-	process(callback: IResponseFn<charges.ICharge>): Customer {
-		stripe.charges.create(this._charge, callback);
-		return this.customer;
-	}
-	
+  /**
+   * Process the transaction
+   * @param { callback: IResponseFn<charges.ICharge> } - Callback to be executed after the transaction has been processed
+   */
+  process(callback?: IResponseFn<charges.ICharge>): Customer | Charge {
+    stripe.charges.create(this._charge, callback);
+    if (this.customer) return this.customer;
+    else return this;
+  }
+  
 }
