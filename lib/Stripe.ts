047351f729536a3a7f654184f5da690e2bcfb8a6
Bennett Hardwick <bennetthardwick@icloud.com>
2018-01-22 17:09:23 +1000

    Forgot I left that there..

--- lib/Stripe.ts
+++ lib/Stripe.ts
@@ -1,10 +1,9 @@
 import * as IStripe from 'stripe';
 export var stripe = null;
 
-var hi = new IStripe("sa");
 /**
   * This function initialises the wrapper and must be called before use
-  * @param key {string} - Stripe private key
+  * @param key: string - Stripe private key
  */
 export function Stripe(key: string): IStripe {
   return stripe = new IStripe(key);
